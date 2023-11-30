8ffa401ff81eee39a81c92053341a274344eb5e7
NguyenQuocKhoi <nguyenquockhoi29122002@gmail.com>
2023-12-01 00:17:53 +0700

    insert update delete company and skill

--- src/main/java/vn/edu/iuh/fit/frontEnd/controllers/CompanyController.java
+++ src/main/java/vn/edu/iuh/fit/frontEnd/controllers/CompanyController.java
@@ -1,5 +1,6 @@
 package vn.edu.iuh.fit.frontEnd.controllers;
 
+import com.neovisionaries.i18n.CountryCode;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -9,8 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import vn.edu.iuh.fit.backEnd.entities.Address;
+import vn.edu.iuh.fit.backEnd.entities.Candidate;
 import vn.edu.iuh.fit.backEnd.entities.Company;
 import vn.edu.iuh.fit.backEnd.repositories.AddressRepository;
 import vn.edu.iuh.fit.backEnd.repositories.CompanyRepository;
@@ -41,5 +49,60 @@ public class CompanyController {
       model.addAttribute("pageNumbers", pageNumbers);
     }
     return "companies/listing";
+  }
+
+  @GetMapping("/companies/show-add-formCompany")
+  public ModelAndView add(Model model) {
+    ModelAndView modelAndView = new ModelAndView();
+    Candidate candidate = new Candidate();
+    Company company = new Company();
+    company.setAddress(new Address());
+    modelAndView.addObject("company",company);
+    modelAndView.addObject("address", company.getAddress());
+    modelAndView.addObject("countries", CountryCode.values());
+    modelAndView.setViewName("companies/addCompany");
+    return modelAndView;
+  }
+
+  @PostMapping("companies/addCompany")
+  public String addCompany(
+      @ModelAttribute("company") Company company,
+      @ModelAttribute("address") Address address,
+      BindingResult result, Model model) {
+    addressRepository.save(address);
+    company.setAddress(address);
+   companyRepository.save(company);
+    return "redirect:/companies";
+  }
+
+  @GetMapping("/show-edit-formCompany/{id}")
+  public ModelAndView edit(@PathVariable("id") long id) {
+    ModelAndView modelAndView = new ModelAndView();
+    Optional<Company> opt = companyRepository.findById(id);
+    if(opt.isPresent()) {
+      Company company = opt.get();
+      modelAndView.addObject("company", company);
+      modelAndView.addObject("address", company.getAddress());
+      modelAndView.addObject("countries", CountryCode.values());
+      modelAndView.setViewName("companies/updateCompany");
+    }
+    return modelAndView;
+  }
+  @PostMapping("companies/updateCompany")
+  public String update(
+      @ModelAttribute("company") Company company,
+      @ModelAttribute("address") Address address,
+      BindingResult result, Model model) {
+    addressRepository.save(address);
+    company.setAddress(address);
+    companyRepository.save(company);
+    return "redirect:/companies";
+  }
+  @GetMapping("/companies/delete/{id}")
+  public String delete(@PathVariable("id") long id) {
+    Company company = companyRepository
+        .findById(id).orElse(new Company());
+    companyRepository.delete(company);
+    return "redirect:/companies";
   }
 }
--- src/main/java/vn/edu/iuh/fit/frontEnd/controllers/SkillController.java
+++ src/main/java/vn/edu/iuh/fit/frontEnd/controllers/SkillController.java
@@ -1,5 +1,6 @@
 package vn.edu.iuh.fit.frontEnd.controllers;
 
+import com.neovisionaries.i18n.CountryCode;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -8,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import vn.edu.iuh.fit.backEnd.entities.Address;
 import vn.edu.iuh.fit.backEnd.entities.Company;
 import vn.edu.iuh.fit.backEnd.entities.Skill;
 import vn.edu.iuh.fit.backEnd.repositories.SkillRepository;
@@ -55,6 +59,32 @@ public class SkillController {
   @PostMapping("/skills/addSkill")
   public String addSkill(@ModelAttribute("skill") Skill skill) {
     skillRepository.save(skill);
+    return "redirect:/skills";
+  }
+
+  @GetMapping("/show-edit-formSkill/{id}")
+  public ModelAndView edit(@PathVariable("id") long id) {
+    ModelAndView modelAndView = new ModelAndView();
+    Optional<Skill> opt = skillRepository.findById(id);
+    if(opt.isPresent()) {
+      Skill skill = opt.get();
+      modelAndView.addObject("skill", skill);
+      modelAndView.setViewName("skills/updateSkill");
+    }
+    return modelAndView;
+  }
+  @PostMapping("skills/updateSkill")
+  public String update(
+      @ModelAttribute("skill") Skill skill,
+      BindingResult result, Model model) {
+    skillRepository.save(skill);
+    return "redirect:/skills";
+  }
+  @GetMapping("/skills/delete/{id}")
+  public String delete(@PathVariable("id") long id) {
+    Skill skill = skillRepository
+        .findById(id).orElse(new Skill());
+    skillRepository.delete(skill);
     return "redirect:/skills";
   }
 }
